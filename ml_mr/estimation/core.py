Added tests to fix the average iv reg when models with uncertainty are used.

ml_mr/estimation/core.py
@@ -65,6 +65,7 @@ class MREstimator(object):
         x: torch.Tensor,
         low_memory: bool = False,
     ) -> torch.Tensor:
+
         if self.covars is None:
             return self.iv_reg_function(x, None)
 
@@ -78,7 +79,8 @@ class MREstimator(object):
         y_hats = self.iv_reg_function(x_rep, covars)
 
         return torch.vstack([
-            tens.mean(dim=0) for tens in torch.split(y_hats, n_covars)
+            tens.mean(dim=0, keepdim=True)
+            for tens in torch.split(y_hats, n_covars)
         ])
 
     def _low_mem_avg_iv_reg_function(self, x: torch.Tensor) -> torch.Tensor:
@@ -89,7 +91,7 @@ class MREstimator(object):
             cur_cf = torch.mean(self.iv_reg_function(
                 cur_x.repeat(num_covars).reshape(num_covars, -1),
                 self.covars
-            ))
+            ), dim=0, keepdim=True)
             avgs.append(cur_cf)
 
         return torch.vstack(avgs)
@@ -488,16 +490,31 @@ class IVDatasetWithGenotypes(IVDataset):
         self.iv_idx_tens = torch.tensor(iv_indices)
         self.covariable_idx_tens = torch.tensor(covariable_indices)
 
+        if self.exposure_index is None:
+            warn(f"Exposure '{exposure_col}' not found in genetic dataset "
+                 f"(will not be accessible).")
+
+        if self.outcome_index is None:
+            warn(f"Outcome '{outcome_col}' not found in genetic dataset "
+                 f"(will not be accessible).")
+
     def __getitem__(self, index: int) -> IVDatasetBatch:
         cur = self.genetic_dataset[index]
-        exposure = cur.exogenous[:, [self.exposure_index]]
-        outcome = cur.exogenous[:, [self.outcome_index]]
+        if self.exposure_index is not None:
+            exposure = cur.exogenous[[self.exposure_index]]
+        else:
+            exposure = torch.Tensor()
+
+        if self.outcome_index is not None:
+            outcome = cur.exogenous[[self.outcome_index]]
+        else:
+            outcome = torch.Tensor()
 
         instruments = cur.dosage
         covars = torch.Tensor()
 
         if self.covariable_idx_tens.numel() > 0:
-            covars = cur.exogenous[:, self.covariable_idx_tens]
+            covars = cur.exogenous[self.covariable_idx_tens]
 
         if self.iv_idx_tens.numel() > 0:
             instruments = torch.hstack(
@@ -511,7 +528,9 @@ class IVDatasetWithGenotypes(IVDataset):
 
     @property
     def covariables(self):
-        self.genetic_dataset.exog[:, self.covariable_idx_tens]
+        covars = self.genetic_dataset.exog[:, self.covariable_idx_tens]\
+            .to(torch.float32)
+        return covars
 
     @property
     def exposure(self):

ml_mr/estimation/quantile_iv.py
@@ -397,9 +397,11 @@ class QuantileIVEstimator(MREstimator):
         self,
         exposure_network: QIVExposureNetType,
         outcome_network: OutcomeMLP,
+        covars: Optional[torch.Tensor] = None
     ):
         self.exposure_network = exposure_network
         self.outcome_network = outcome_network
+        super().__init__(covars)
 
     def iv_reg_function(
         self, x: torch.Tensor, covars: Optional[torch.Tensor] = None
@@ -411,6 +413,11 @@ class QuantileIVEstimator(MREstimator):
     def from_results(cls, dir_name: str) -> "QuantileIVEstimator":
         with open(os.path.join(dir_name, "meta.json"), "rt") as f:
             meta = json.load(f)
+
+        try:
+            covars = torch.load(os.path.join(dir_name, "covariables.pt"))
+        except FileNotFoundError:
+            covars = None
 
         exposure_net_cls: type[pl.LightningModule] = (
             ExposureNMQN if meta.get("nmqn", False)
@@ -442,7 +449,8 @@ class QuantileIVEstimator(MREstimator):
             return QuantileIVEstimatorWithUncertainty(
                 exposure_network,
                 outcome_network,
-                meta
+                meta,
+                covars
             )
 
         return cls(exposure_network, outcome_network)
@@ -456,10 +464,12 @@ class QuantileIVEstimatorWithUncertainty(
         self,
         exposure_network: QIVExposureNetType,
         outcome_network: OutcomeMLP,
-        meta: dict
+        meta: dict,
+        covars: Optional[torch.Tensor] = None,
     ):
         self.exposure_network = exposure_network
         self.outcome_network = outcome_network
+        self.covars = covars
 
         # Conformal prediction adjustment.
         self.conformal_score: NONCONFORMITY_MEASURES_TYPE =\
@@ -701,6 +711,7 @@ def fit_quantile_iv(
     meta["model"] = "quantile_iv"
     meta.update(dataset.exposure_descriptive_statistics())
     del meta["dataset"]  # We don't serialize the dataset.
+    del meta["stage2_dataset"]
 
     covars = dataset.save_covariables(output_dir)
 
@@ -787,16 +798,18 @@ def fit_quantile_iv(
         fit_conformal(
             outcome_network,
             conformal_score,
-            val_dataset,  # type: ignore
+            stg2_val_dataset,  # type: ignore
             meta,
             alpha=conformal_alpha_level
         )
 
         estimator: QuantileIVEstimator = QuantileIVEstimatorWithUncertainty(
-            exposure_network, outcome_network, meta
+            exposure_network, outcome_network, meta, covars
         )
     else:
-        estimator = QuantileIVEstimator(exposure_network, outcome_network)
+        estimator = QuantileIVEstimator(
+            exposure_network, outcome_network, covars
+        )
 
     # Save the metadata, estimator statistics and log artifact to WandB if
     # required.
@@ -806,10 +819,8 @@ def fit_quantile_iv(
     if not fast:
         save_estimator_statistics(
             estimator,
-            covars,
             domain=meta["domain"],
             output_prefix=os.path.join(output_dir, "causal_estimates"),
-            alpha=conformal_alpha_level
         )
 
     if wandb_project is not None:
@@ -914,17 +925,14 @@ def plot_exposure_model(
 
 def save_estimator_statistics(
     estimator: QuantileIVEstimator,
-    covars: Optional[torch.Tensor],
     domain: Tuple[float, float],
     output_prefix: str = "causal_estimates",
-    alpha: Optional[float] = None
 ):
     # Save the causal effect at over the domain.
     xs = torch.linspace(domain[0], domain[1], 500).reshape(-1, 1)
 
     if isinstance(estimator, QuantileIVEstimatorWithUncertainty):
-        assert alpha is not None
-        ys = estimator.iv_reg_function(xs, covars, alpha=alpha)
+        ys = estimator.avg_iv_reg_function(xs)
 
         if ys.size(1) != 1:
             raise NotImplementedError(
@@ -938,7 +946,7 @@ def save_estimator_statistics(
         )
 
     else:
-        ys = estimator.iv_reg_function(xs, covars).reshape(-1)
+        ys = estimator.avg_iv_reg_function(xs).reshape(-1)
         df = pd.DataFrame({"x": xs.reshape(-1), "y_do_x": ys})
 
     plt.figure()
@@ -946,14 +954,13 @@ def save_estimator_statistics(
 
     if "y_do_x_lower" in df.columns:
         # Add the CI on the plot.
-        assert alpha is not None
         plt.fill_between(
             df["x"],
             df["y_do_x_lower"],
             df["y_do_x_upper"],
             color="#dddddd",
             zorder=-1,
-            label=f"{int((1 - alpha) * 100)}% Prediction interval"
+            label="Prediction interval"
         )
 
     plt.xlabel("X")

ml_mr/tests/estimation/fixtures.py
@@ -3,7 +3,7 @@ from typing import Optional, Callable
 import pytest
 import torch
 
-from ...estimation.core import MREstimator
+from ...estimation.core import MREstimator, MREstimatorWithUncertainty
 
 
 class MREstimatorFromFunction(MREstimator):
@@ -21,6 +21,27 @@ class MREstimatorFromFunction(MREstimator):
         covars: Optional[torch.Tensor] = None
     ) -> torch.Tensor:
         return self.f(x, covars)
+
+
+class MREstimatorWithUncertaintyFromFunction(
+    MREstimatorWithUncertainty
+):
+    def __init__(
+        self,
+        f: Callable[[torch.Tensor, Optional[torch.Tensor]], torch.Tensor],
+        covars: Optional[torch.Tensor] = None
+    ):
+        super().__init__(covars)
+        self.f = f
+
+    def iv_reg_function(
+        self,
+        x: torch.Tensor,
+        covars: Optional[torch.Tensor] = None,
+        alpha: float = 0.1,
+    ) -> torch.Tensor:
+        out = self.f(x, covars)
+        return torch.cat((out - 0.1, out, out + 0.1), dim=1).reshape(-1, 1, 3)
 
 
 @pytest.fixture
@@ -44,3 +65,19 @@ def mr_estimator_identity_elem_product_covars():
         return x * covars
 
     return MREstimatorFromFunction(f)
+
+
+@pytest.fixture
+def mr_estimator_covar_itx_with_unc():
+    """Implements Y = X * C where C are the covariates and simulates
+    uncertainty.
+
+    This is to simulate the output of a MREstimatorWithUncertainty.
+
+    """
+    def f(x, covars):
+        assert covars is not None
+        assert x.shape == covars.shape
+        return x * covars
+
+    return MREstimatorWithUncertaintyFromFunction(f)